7d00253a44892bd6e7c34714c7a83a2d4f880acc
Norbert Benkó <rongybaba12@gmail.com>
2019-06-26 21:44:23 +0200

    Fixed variable names

--- TicTacToe_f3ny3v0k.py
+++ TicTacToe_f3ny3v0k.py
@@ -2,48 +2,48 @@ import os, time, random
 
 def main():
     board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    winnerPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    gameOver = 0
-    replayRequest = ""
-    redX = "\033[91mx\033[00m"
-    greenO = "\033[92mo\033[00m"
-    while(replayRequest != "n"):
+    winner_positions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    game_over = 0
+    replay_request = ""
+    red_x = "\033[91mx\033[00m"
+    green_o = "\033[92mo\033[00m"
+    while(replay_request != "n"):
         while(True): 
             cls()
             Print(board)
-            userInput(board, greenO, redX)
+            user_input(board, green_o, red_x)
             cls()
             Print(board)
-            gameOver = winnerPosition(board, winnerPositions, gameOver, greenO, redX)
-            if gameOver == 1 or gameOver == 3:
-                if gameOver == 1: 
+            game_over = winner_position(board, winner_positions, game_over, green_o, red_x)
+            if game_over == 1 or game_over == 3:
+                if game_over == 1: 
                     print("Congratulations, you won!")
-                elif gameOver == 3:
+                elif game_over == 3:
                     print("It\'s a tie")
                 break
-            AI(board, winnerPositions, greenO, redX)
+            AI(board, winner_positions, green_o, red_x)
             cls()
             Print(board)
-            gameOver = winnerPosition(board, winnerPositions, gameOver, greenO, redX)
-            if gameOver == 2 or gameOver == 3:
-                if gameOver == 2: 
+            game_over = winner_position(board, winner_positions, game_over, green_o, red_x)
+            if game_over == 2 or game_over == 3:
+                if game_over == 2: 
                     print("Game Over! The computer won!")
-                if gameOver ==3:
+                if game_over ==3:
                     print("It\'s a tie")
                 break
-        replayRequest = input("Do you want to play another round? (y/n):")
+        replay_request = input("Do you want to play another round? (y/n):")
         for x in range(len(board)): 
             board[x] = x 
-        gameOver = 0
+        game_over = 0
 
 
 def Print(board):
-    dash = "-"*7
-    divider = "|"
+    dash = "+-+-+-+"
+    separator = "|"
     for x in range(len(board)):
         if x == 0:
             print(dash)
-        print(divider + str(board[x]), end="")
+        print(f"{separator}{board[x]}", end="")
         if x == 2 or x == 5 or x == 8:
             print("|")
             print(dash)
@@ -53,70 +53,70 @@ def cls():
     os.system('cls' if os.name == 'nt' else 'clear')  
 
 
-def userInput(board, greenO, redX):
+def user_input(board, green_o, red_x):
     while(True):
         try:
-            enterNum = int(input("Please enter the number: "))
+            enter_num = int(input("Please enter the number: "))
         except ValueError:
             print("Use only numbers please!")
             continue
-        if enterNum not in board:
+        if enter_num not in board:
             print("Invalid position!")
             continue
-        elif board[enterNum] == redX or board[enterNum] == greenO:
+        elif board[enter_num] == red_x or board[enter_num] == green_o:
             print("That position is already taken!")
             continue
         else:
-            board[enterNum] = redX
+            board[enter_num] = red_x
             break
 
 
-def winnerPosition(board, winnerPositions, gameOver, greenO, redX):
-    for x in winnerPositions:
-        if board[x[0]] == redX and board[x[1]] == redX and board[x[2]] == redX:
-            gameOver = 1
+def winner_position(board, winner_positions, game_over, green_o, red_x):
+    for x in winner_positions:
+        if board[x[0]] == red_x and board[x[1]] == red_x and board[x[2]] == red_x:
+            game_over = 1
             break
-        if board[x[0]] == greenO and board[x[1]] == greenO and board[x[2]] == greenO:
-            gameOver = 2
+        if board[x[0]] == green_o and board[x[1]] == green_o and board[x[2]] == green_o:
+            game_over = 2
             break
-        if board.count(redX) == 5:
-            gameOver = 3
-    return gameOver
+        if board.count(red_x) == 5:
+            game_over = 3
+    return game_over
 
 
-def AI(board, winnerPositions, greenO, redX):
+def AI(board, winner_positions, green_o, red_x):
     time.sleep(.500)
     marker = 0
-    for x in winnerPositions:
-        if [board[x[0]], board[x[1]], board[x[2]]].count(greenO) == 2 and any(isinstance(y, int) for y in [board[x[0]], board[x[1]], board[x[2]]]):
+    for x in winner_positions:
+        if [board[x[0]], board[x[1]], board[x[2]]].count(green_o) == 2 and any(isinstance(y, int) for y in [board[x[0]], board[x[1]], board[x[2]]]):
             for y in range(len(x)):
                 if isinstance(board[x[y]], int) and marker == 0:
-                    board[x[y]] = greenO
+                    board[x[y]] = green_o
                     marker = 1
     if marker == 0:
-        for x in winnerPositions:
-            if [board[x[0]], board[x[1]], board[x[2]]].count(redX) == 2 and any(isinstance(y, int) for y in [board[x[0]], board[x[1]], board[x[2]]]):
+        for x in winner_positions:
+            if [board[x[0]], board[x[1]], board[x[2]]].count(red_x) == 2 and any(isinstance(y, int) for y in [board[x[0]], board[x[1]], board[x[2]]]):
                 for y in range(len(x)):
                     if isinstance(board[x[y]], int) and marker == 0:
-                        board[x[y]] = greenO
+                        board[x[y]] = green_o
                         marker = 1
     if marker == 0: 
-        for x in winnerPositions:
-            if [board[x[0]], board[x[1]], board[x[2]]].count(greenO) == 1 and redX not in [board[x[0]], board[x[1]], board[x[2]]]:
+        for x in winner_positions:
+            if [board[x[0]], board[x[1]], board[x[2]]].count(green_o) == 1 and red_x not in [board[x[0]], board[x[1]], board[x[2]]]:
                 for y in range(len(x)):
-                    if board[x[y]] != greenO and marker == 0:
-                        board[x[y]] = greenO
+                    if board[x[y]] != green_o and marker == 0:
+                        board[x[y]] = green_o
                         marker = 1
     if marker == 0: 
         while(True):
             if isinstance(board[4], int):
-                board[4] = greenO
+                board[4] = green_o
                 break
-            randomNum = random.randint(0, 8)
-            if board[randomNum] == redX or board[randomNum] == greenO: 
+            random_num = random.randint(0, 8)
+            if board[random_num] == red_x or board[random_num] == green_o: 
                 continue
             else: 
-                board[randomNum] = greenO
+                board[random_num] = green_o
                 break
        
 
